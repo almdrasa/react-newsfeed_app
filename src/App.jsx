eb2ba60713634f50a956b7e5528441541d170bbd
Ahmed Ali <a.husseinzaki@gmail.com>
2023-10-08 11:49:51 -0700

    feat: Support abort old requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import { debounce } from "lodash";
 import Container from "@mui/material/Container";
 import NewsHeader from "./components/NewsHeader";
@@ -9,12 +9,17 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState("");
 
+  const abortController = useRef(new AbortController());
+
   async function loadData(query) {
     const baseEndpoint = `https://newsapi.org/v2/top-headlines?country=eg&apiKey=${
       import.meta.env.VITE_NEWS_API_KEY
     }`;
     const response = await fetch(
-      query ? `${baseEndpoint}&q=${query}` : baseEndpoint
+      query ? `${baseEndpoint}&q=${query}` : baseEndpoint,
+      {
+        signal: abortController.current.signal,
+      }
     );
     const data = await response.json();
     return data.articles.map((article) => {
@@ -38,6 +43,9 @@ function App() {
   const fetchArticles = useCallback(
     debounce(() => {
       setLoading(true);
+      abortController.current.abort();
+      abortController.current = new AbortController();
+
       loadData(query)
         .then((articles) => {
           setLoading(false);
@@ -53,6 +61,11 @@ function App() {
 
   useEffect(() => {
     fetchArticles();
+
+    return () => {
+      // Cleanup: Abort any pending requests
+      abortController.current.abort();
+    };
   }, [query]);
 
   const handleSearchChange = (query) => {
