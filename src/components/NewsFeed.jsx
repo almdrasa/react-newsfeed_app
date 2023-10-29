cfd912c817a567b6c437f47365f63a1d6779b1d8
Ahmed Ali <a.husseinzaki@gmail.com>
2023-10-29 10:33:57 -0700

    fix: Missing key warning for LoadingArticle array

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -11,7 +11,8 @@ function NewsFeed(props) {
   const { articles, loading, pageSize } = props;
   return (
     <Box>
-      {loading && [...Array(pageSize)].map(() => <LoadingArticle />)}
+      {loading &&
+        [...Array(pageSize)].map((_, index) => <LoadingArticle key={index} />)}
       {!loading && articles.length === 0 && (
         <Typography
           align="center"
